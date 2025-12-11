4924f43bb6748ec5d320e53b2b0f700c6b3bb5f0
reminifatimaMF <reminifatima8@gmail.com>
2025-12-11 22:24:27 +0100

    Update ReminifatimaMF.py

--- ReminifatimaMF.py
+++ ReminifatimaMF.py
@@ -46,7 +46,7 @@ average_gc = df["Pourcentage GC"].mean()
 print(f"Pourcentage moyen de GC:{average_gc:.3f}%")
 
 #5) Ajouter une nouvelle colonne "Catégorie GC"
-print("************* Ajouter une nouvelle colonne "catégorie GC" *************")
+print("************* Ajouter une nouvelle colonne 'catégorie GC' *************")
 #Ajouter une nouvelle colonne "catégorie GC"
 df["Catégorie GC"] = df["Pourcentage GC"].apply(lambda x: "Riche" if x > 55 else ("Moyen" if 45 <= x <= 55 else "Faible"))
 print(df)

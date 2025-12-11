fa2797120c43bac06c04d4587082c5f332320a39
nafissa00 <m95smnh9w9@privaterelay.appleid.com>
2025-12-11 18:42:43 +0100

    Filter sequences by length and calculate average GC
    Added filtering for sequences longer than 10 and calculated average GC percentage.

--- ReminifatimaMF.py
+++ ReminifatimaMF.py
@@ -33,3 +33,13 @@ print("************* Affichage de la colonne Longueur *************\n")
 
 col_longueur = df["Longueur"]
 print(col_longueur)
+#3) filrer les séquences avec la longueur est supérieurr a 10%
+print("*********** Filtrage Avec pourcentage % ***********") 
+Filtrer les séquences avec la longueur est supérieur à 10
+filtered_df=df[df["Longueur"]>10]
+print(filtered_df)
+#4) calculer la moyenne du pourcentage de GC aprés la virgule 
+print("*********** Calcul de la moyenne ***********")
+Calculer la moyenne du pourcentage de GC Avec 3 chifrre aprés la virgule
+Average_gc = df["pourcentage GC"].mean()
+print(f"Pourcentage moyen de GC:{average_gc:.3f}%")
